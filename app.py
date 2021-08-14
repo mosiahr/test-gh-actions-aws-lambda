9afa68eab3f76373185746f3d5855a93ac6d4052
Gregory Mosia <greg.mosia@gmail.com>
2021-08-14 12:09:24 +0300

    Add circular buffer

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import collections
 from typing import Optional
 
 import requests
@@ -20,19 +19,51 @@ def get_json_response(url: str) -> Optional[dict]:
 
 def get_list_of_sort_title() -> Optional[list]:
     json_response = get_json_response(URL_API)
-    
+
     if json_response:
         list_fields = [record['fields'] for record in json_response['records']]
         sorted_list_fields = sorted(list_fields, key=lambda k: k['ID'])
         return [ field['title'] for field in sorted_list_fields ]
 
+
+class CircularBuffer:
+    def __init__(self, size) -> None:
+        self.max = size
+        self.data = []
+        
+    def __len__(self) -> int:
+        return self.data.__len__()
+
+    def append(self, el) -> None:
+        self.data.append(el)
+
+        if len(self.data) == self.max:
+            self.cur = 0
+
+            #  Permanently change self's class from non-full to full
+            self.__class__ = self.__Full
+    
+    def get(self) -> list:
+        return self.data
+
+    class __Full:
+        """ Class that implements a full buffer """
+        def __len__(self) -> int:
+            return self.data.__len__()
+
+        def append(self, el) -> None:
+            self.data[self.cur] = el
+            self.cur = (self.cur + 1) % self.max
+
+        def get(self) -> list:
+            return self.data[self.cur:] + self.data[:self.cur]
+            
+
+
 def main() -> None:
     titles = get_list_of_sort_title()
-    for title in titles:
-        print(title)
-
+    print(titles)
 
 
 if __name__ == '__main__':
     main()
-    
--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,23 @@
-from app import main
+from app import get_list_of_sort_title, CircularBuffer
+
+
+circular_buffer_res = CircularBuffer(3)
+list_i = [0]
 
 def lambda_handler(event, context):
-    return main()
+    titles = get_list_of_sort_title()
+    i = list_i.pop()
+    
+    while len(circular_buffer_res) < 2:
+        circular_buffer_res.append(titles[i])
+        i = i + 1
+    
+    circular_buffer_res.append(titles[i])
+    
+    if i < len(titles) - 1:
+        list_i.append(i + 1)
+    else:
+        i = 0
+        list_i.append(i)
+    
+    return circular_buffer_res.get()
